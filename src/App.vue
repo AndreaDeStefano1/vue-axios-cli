075edb3537e0cc7dd092581fad423f9ec5bfaa8b
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-05-17 12:48:14 +0200

    Esercizio in classe

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container" v-if="isLoaded">
+      <CoffeeComp
+      v-for="coffee in coffeeArray" :key="coffee.id"
+      :coffeItem="coffee"/>
+    </div>
+    <div class="container" v-else>
+      <LoaderComp loaderName="LOADING CAFFE"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import CoffeeComp from './components/CoffeeComp.vue';
+import LoaderComp from './components/LoaderComp.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      baseURL:'https://api.sampleapis.com/coffee/hot',
+      coffeeArray:[],
+      isLoaded : false
+    }
+  },
   components: {
-    HelloWorld
+    CoffeeComp,
+    LoaderComp
+},
+  mounted(){
+    this.getAPI()
+  },
+  methods:{
+    getAPI(){
+     axios.get(this.baseURL)
+     .then(r => {
+       this.coffeeArray = r.data;
+       console.log(r.data);
+        this.isLoaded = true
+     })
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style/general'
+
 </style>
---
+++ src/components/CoffeeComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+    <h3>{{coffeItem.title}}</h3>
+    <p>{{coffeItem.description}}</p>
+    <ul>
+      <h4>Ingredienti:</h4>
+      <li v-for="(ingredient,index) in coffeItem.ingredients" :key="`ingredient-${index}`">{{ingredient}} </li>
+     
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoffeeComp',
+  props:{
+    coffeItem : Object
+  }
+}
+
+</script>
+
+<style>
+
+</style>
